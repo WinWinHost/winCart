aed46be99c2cd0bc20901279bbe1636162461e93
Muhammad Saqib <rooott@gmail.com>
2011-08-03 10:32:25 +0500

    initial commit

--- application.BACKUP.5995/models/T.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Model_T extends Zend_Db_Table_Abstract { 
-    
-   protected  $_name='t';
-    
-}
--- application.BACKUP.5995/modules/contact/.svn/text-base/Bootstrap.php.svn-base
+++
@@ -1,1 +0,0 @@
-<?php class Contact_Bootstrap extends Zend_Application_Module_Bootstrap {} ?>
