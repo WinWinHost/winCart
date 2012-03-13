6e01a7213c2d6b4c8651eccb98d6a9b4976742f6
gugas <darius.lenkauskis@yahoo.com>
2012-03-13 18:56:17 +0200

    and acl resource 'procuct' error fix

---
+++ application/configs/database.php
@@ -0,0 +1,14 @@
+<?php
+return array (
+  'database' => 
+  array (
+    'adapter' => 'Pdo_Mysql',
+    'params' => 
+    array (
+      'host' => 'localhost',
+      'dbname' => 'winwinhost1',
+      'username' => 'root',
+      'password' => '',
+    ),
+  ),
+);
